613cf3953cf49b1f8241e081240878bef651108b
kimsoomin <skno1382@soongsil.ac.kr>
2024-09-08 00:51:12 +0900

    [9.6] boj 2293

--- boj_solve/2293.cpp
+++ boj_solve/2293.cpp
@@ -1,32 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
+int v[101];
+long d[10001];
+int n, k;
+
+int main() {
     cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
 
-    int n; int k;
     cin >> n >> k;
-    int d[k+1];
-    d[0] = 0;
-    int val[n];
-    for (int i=0; i<n; i++){
-        cin >> val[i];
-    }
 
-    for (int i=1; i<k+1; i++){
-        d[i] = 1;
-        for (int j=0; j<n; j++){
-            if (i>=val[j]){
-                d[i] += d[i-val[j]];
-            }
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+    d[0] = 1;
+    for(int i = 1; i <=n; i++){
+        for(int j = v[i]; j <= k; j++){
+            d[j] = d[j] + d[j-v[i]];
         }
-        
     }
-    
-    for (int i=0; i<k+1; i++){
-        cout << d[i];
-    }
-    cout << "#" << d[k];
-    
+    cout << d[k];
 }
