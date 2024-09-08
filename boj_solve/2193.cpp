4507cde7ac94ae5c190729e8865d0ba7c992b3c7
kimsoomin <skno1382@soongsil.ac.kr>
2024-09-08 14:31:31 +0900

    [9.8] boj 2193

---
+++ boj_solve/2193.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N; 
+long oneDP[93];
+long zeroDP[93];
+
+int main() {
+    cin >> N;
+
+    for (int i=1; i<=N; i++) {
+        if (i == 1) {
+            zeroDP[i] = 0;
+            oneDP[i] = 1; 
+        } else {
+            zeroDP[i] = zeroDP[i-1] + oneDP[i-1];
+            oneDP[i] = zeroDP[i-1];
+        }
+    }
+
+    cout << oneDP[N] + zeroDP[N];
+}
