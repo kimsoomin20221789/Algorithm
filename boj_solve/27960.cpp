3b91f62c34bdcb2ea66a334950fa60ec7901a810
kimsoomin <skno1382@soongsil.ac.kr>
2025-01-29 00:52:31 +0900

    [1.29] boj 27960

---
+++ boj_solve/27960.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int x; int y; cin >> x >> y;
+    int a = x^y;
+    cout << a;
+}
