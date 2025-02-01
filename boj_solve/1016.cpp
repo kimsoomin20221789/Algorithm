40f3777490ecb9834af33b68d5f4741291de8c46
kimsoomin <skno1382@soongsil.ac.kr>
2025-02-01 17:08:32 +0900

    [2.1] boj 1016

--- boj_solve/1016.cpp
+++ boj_solve/1016.cpp
@@ -1,25 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main(){
-    int min;
-    int max;
-    cin >> min >> max;
-    int size1 = max-min;
+    ll min_val, max_val;
+    cin >> min_val >> max_val;
+    ll rangeSize = max_val - min_val + 1;
 
-    bool isErased[size1];
-    int cnt = 0;
-    if (min == 1){
-        isErased[0] = true;
-    }
-    for(int p=min; p*p <=max; ++p){
-        if(isErased[p-min]) continue;
-        for(int j=p*p; j<=max; j+=p){
-            isErased[j-min]=true;
-            cout << j-min+1 << "&&";
-            cnt ++;
+    // 제곱수 배수 여부를 체크할 배열(각 인덱스는 해당 범위의 한 숫자에 대응)
+    bool* isSquareMultiple = new bool[rangeSize + 1]{0};
+    // 2 이상 sqrt(max_val) 이하의 모든 i에 대해 i^2의 배수를 체크
+    for (ll i = 2; i * i <= max_val; i++) {
+        ll square = i * i;
+
+        // min_val 이상이면서 square로 나누어떨어지는 최소 시작값
+        ll start = ((min_val - 1) / square + 1) * square;
+
+        // start부터 max_val 이하까지 square씩 증가
+        for (ll j = start; j <= max_val; j += square) {
+            isSquareMultiple[j - min_val] = true;
         }
     }
-    cout << cnt;
+
+    // 제곱수의 배수가 아닌(아직 false인) 인덱스 개수
+    ll result = 0;
+    for (ll i = 0; i < rangeSize; i++) {
+        if (!isSquareMultiple[i]) {
+            result++;
+        }
+    }
+    delete[] isSquareMultiple;
+    cout << result << "\n";
+    return 0;
 }
