66155ec5eff7ea76cbc6561588e650dcb392751a
kimsoomin <skno1382@soongsil.ac.kr>
2025-02-02 11:57:34 +0900

    [2.2]  boj 6131

--- boj_solve/6131.cpp
+++ boj_solve/6131.cpp
@@ -1,39 +1,16 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPowerOfTwo(int n) {
-    if (n <= 0) {
-        return false;  // 음수나 0은 거듭제곱이 아님
-    }
-    
-    while (n > 1) {
-        if (n % 2 != 0) {
-            return false;  // 2로 나누어 떨어지지 않으면 거듭제곱이 아님
+int main() {
+    int N; cin >> N;
+    int ans = 0;
+    for (int b=1; b<=500; b++) {
+        for (int a=b+1; a<=500; a++) {
+            if (a*a - b*b == N ) {
+                ans++;
+            }
         }
-        n /= 2;
     }
-    
-    return true;  // 마지막에 1이 되면 거듭제곱
-}
 
-
-int main(){
-    int K = 500;
-    int N;  cin >> N;
-    int cnt = 0;
-    int a;
-    for (int i=1; i<=sqrt(K); i++){
-        a = i*i + N;
-        if (a > 500){
-            break;
-        }
-        if (isPowerOfTwo(a)){
-            cnt += 1;
-        }
-        else {
-            continue;
-        }
-    }
-    cout << cnt;
+    cout << ans;
 }
