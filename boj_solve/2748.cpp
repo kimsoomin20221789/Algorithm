b7bf9c71569f46f35061690852c263800540cbf2
kimsoomin <skno1382@soongsil.ac.kr>
2025-02-03 14:46:41 +0900

    [2.3] boj 2748

--- boj_solve/2748.cpp
+++ boj_solve/2748.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 int N;
-int dp[91]; //dp는 저장을 하는 공간! (memorization)
-
-
-int fib(int a){
-    if (dp[a]!=-1) {return dp[a];}
-    return dp[a] = fib(a-1) + fib(a-2);
-}
+long long dp[93];
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
     cin >> N;
-    for (int i=0; i<91; i++){
-        dp[i] = -1;
-    }   
+
     dp[0] = 0;
     dp[1] = 1;
-    cout << fib(N);
+    for (int i=2; i<=N; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }   
+
+    cout << dp[N];
 }
